fd0ba832168a13c6e29e0d83898ef50c3d3462bb
keiko <nuannuanbaise@163.com>
2021-07-01 02:57:14 +0800

    first commit

---
+++ src/dom.js
@@ -0,0 +1,25 @@
+window.dom = {
+    find(selector, scope) {
+        return (scope || document).querySelectorAll(selector)
+    },
+    style(node, name, value) {
+        if (arguments.length === 3) {
+            node.style[name] = value
+        } else if (arguments.length === 2) {
+            if (typeof name === 'string') {
+                return node.style[name]
+            } else if (name instanceof Object) {
+                const Object = name
+                for (let key in Object) {
+                    node.style[key] = Object[key]
+                }
+            }
+
+        }
+    },
+    each(nodeList, fn) {
+        for (let i = 0; i < nodeList.length; i++) {
+            fn.call(null, nodeList[i])
+        }
+    }
+}
